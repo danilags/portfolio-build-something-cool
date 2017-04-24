6f3f5962d8a098f0d934d2474d6c1c3329491932
danilags <aguspermadi94@gmail.com>
2017-04-24 16:42:01 +0700

    done

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -54,7 +54,7 @@ export default new Router({
       component: NewArticle,
       beforeEnter: (to, from, next) => {
         if (window.localStorage.getItem('token') && window.localStorage.getItem('username')) {
-          next(username)
+          next()
         } else {
           next(false)
         }
